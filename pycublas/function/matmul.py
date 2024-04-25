27afbe5af63d56b442b8aa14a184fcc2c0e18e38
Your Name <you@example.com>
2024-04-25 06:04:17 +0000

    Refactor matmul.py to add matmul_nn_bf16_fp32 function

--- pycublas/function/matmul.py
+++ pycublas/function/matmul.py
@@ -8,6 +8,8 @@ class matmul_nt_bf16_fp32(torch.autograd.Function):
     @staticmethod
     def forward(ctx, A, B, C=None):
         ctx.save_for_backward(A, B)
+        assert A.is_contiguous()
+        assert B.is_contiguous()
         assert A.dtype == torch.bfloat16
         assert B.dtype == torch.bfloat16
         assert A.device == B.device
@@ -17,10 +19,11 @@ class matmul_nt_bf16_fp32(torch.autograd.Function):
         K = A.shape[1]
         assert K == B.shape[1]
         if C is None:
-            C = torch.empty(
+            C = torch.zeros(
                 A.shape[0], B.shape[0], dtype=torch.float32, device=A.device
             )
         else:
+            assert C.is_contiguous()
             assert C.dtype == torch.float32
             assert C.device == A.device
             assert C.shape == (M, N)
@@ -37,6 +40,56 @@ class matmul_nt_bf16_fp32(torch.autograd.Function):
             B.data_ptr(),
             "CUDA_R_16BF",
             K,
+            A.data_ptr(),
+            "CUDA_R_16BF",
+            K,
+            pycublas.constant.float32_one,
+            C.data_ptr(),
+            "CUDA_R_32F",
+            N,
+            "CUBLAS_COMPUTE_32F",
+            "CUBLAS_GEMM_DEFAULT_TENSOR_OP",
+        )
+
+        return C
+
+
+class matmul_nn_bf16_fp32(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, A, B, C=None):
+        ctx.save_for_backward(A, B)
+        assert A.is_contiguous()
+        assert B.is_contiguous()
+        assert A.dtype == torch.bfloat16
+        assert B.dtype == torch.bfloat16
+        assert A.device == B.device
+        assert A.device.type == "cuda"
+        M = A.shape[0]
+        N = B.shape[1]
+        K = A.shape[1]
+        assert K == B.shape[0]
+        if C is None:
+            C = torch.zeros(
+                A.shape[0], B.shape[1], dtype=torch.float32, device=A.device
+            )
+        else:
+            assert C.is_contiguous()
+            assert C.dtype == torch.float32
+            assert C.device == A.device
+            assert C.shape == (M, N)
+        handle = torch.cuda.current_blas_handle()
+
+        cublas.cublasGemmEX(
+            handle,
+            0,
+            0,
+            N,
+            M,
+            K,
+            pycublas.constant.float32_one,
+            B.data_ptr(),
+            "CUDA_R_16BF",
+            N,
             A.data_ptr(),
             "CUDA_R_16BF",
             K,
