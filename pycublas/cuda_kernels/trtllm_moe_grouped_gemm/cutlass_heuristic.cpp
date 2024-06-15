0f885c2add421d635ab0c0e572545ecbe79d5907
Wenxiang <8460860+wenxcs@users.noreply.github.com>
2024-06-15 13:58:04 +0800

    disable torch flag (#4)

--- pycublas/cuda_kernels/trtllm_moe_grouped_gemm/cutlass_heuristic.cpp
+++ pycublas/cuda_kernels/trtllm_moe_grouped_gemm/cutlass_heuristic.cpp
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+// DISABLE Pytorch CUDAExtension Flags
+#undef __CUDA_NO_HALF_CONVERSIONS__ 
+#undef __CUDA_NO_HALF_OPERATORS__
+#undef __CUDA_NO_BFLOAT16_CONVERSIONS__
+#undef __CUDA_NO_HALF2_OPERATORS__
+
 #include "cutlass_heuristic.h"
 #include "tensorrt_llm/common/cudaBf16Wrapper.h"
 
